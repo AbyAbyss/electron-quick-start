d11580da5c9c59742c6a8d43ce386d0df2ba727d
Aby Saji <abyrocksaji@gmail.com>
2020-11-25 13:50:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
+      nodeIntegration: true,
+      webviewTag: true,
+      nativeWindowOpen: true,
       preload: path.join(__dirname, 'preload.js')
     }
   })
